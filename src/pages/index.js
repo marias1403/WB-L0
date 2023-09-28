0fb4409d016b65938ec7f32a4d5f2782d16b7e50
Maria Arkhipova <marias1403@yandex.ru>
2023-09-28 20:27:57 +0900

    fix: links

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,15 @@ import { validationConfig } from "../utils/validationConfig";
 import { emptyFieldErrorMessages } from "../utils/emptyFieldErrorMessages";
 import { invalidEmailErrorMessages } from "../utils/invalidEmailErrorMessages";
 import {
-  editPaymentButton,
-  editDeliveryButton,
-  dropdownButton1,
+  btnEditPaymentText,
+  btnEditPaymentIcon,
+  btnEditDeliveryText,
+  btnEditDeliveryIcon,
+  btnDropdown1,
   dropdownContent1,
   selectAllCheckbox,
   dropdownTitle1,
-  dropdownButton2,
+  btnDropdown2,
   dropdownContent2,
   dividerLine2
 } from "../utils/constants";
@@ -41,37 +43,43 @@ function toggleDropdown1() {
     dropdownContent1.style.display = "none";
     selectAllCheckbox.style.display = "none";
     dropdownTitle1.classList.add("cart-contents__title_block");
-    dropdownButton1.classList.add("dropdown__button_closed");
+    btnDropdown1.classList.add("dropdown__button_closed");
   } else {
     dropdownContent1.style.display = "flex";
     selectAllCheckbox.style.display = "flex";
     dropdownTitle1.classList.remove("cart-contents__title_block");
-    dropdownButton1.classList.remove("dropdown__button_closed");
+    btnDropdown1.classList.remove("dropdown__button_closed");
   }
 }
-dropdownButton1.addEventListener("click", toggleDropdown1);
+btnDropdown1.addEventListener("click", toggleDropdown1);
 
 function toggleDropdown2() {
   if (dropdownContent2.style.display === "flex") {
     dropdownContent2.style.display = "none";
     dividerLine2.style.display = "none";
-    dropdownButton2.classList.add("dropdown__button_closed");
+    btnDropdown2.classList.add("dropdown__button_closed");
   } else {
     dropdownContent2.style.display = "flex";
     dividerLine2.style.display = "block";
-    dropdownButton2.classList.remove("dropdown__button_closed");
+    btnDropdown2.classList.remove("dropdown__button_closed");
   }
 }
-dropdownButton2.addEventListener("click", toggleDropdown2);
+btnDropdown2.addEventListener("click", toggleDropdown2);
 
 const paymentPopup = new PaymentPopup(".popup_type_payment-method");
 paymentPopup.setEventListeners();
 const deliveryPopup = new DeliveryPopup(".popup_type_delivery-method");
 deliveryPopup.setEventListeners();
-editPaymentButton.addEventListener("click", () => {
+btnEditPaymentText.addEventListener("click", () => {
   paymentPopup.open();
 });
-editDeliveryButton.addEventListener("click", () => {
+btnEditPaymentIcon.addEventListener("click", () => {
+  paymentPopup.open();
+});
+btnEditDeliveryText.addEventListener("click", () => {
+  deliveryPopup.open();
+});
+btnEditDeliveryIcon.addEventListener("click", () => {
   deliveryPopup.open();
 });
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,13 @@
-export const editPaymentButton = document.getElementById("editPaymentButton");
-export const editDeliveryButton = document.getElementById("editDeliveryButton");
+export const btnEditPaymentText = document.getElementById("btnEditPaymentText");
+export const btnEditPaymentIcon = document.getElementById("btnEditPaymentIcon");
+export const btnEditDeliveryText = document.getElementById("btnEditDeliveryText");
+export const btnEditDeliveryIcon = document.getElementById("btnEditDeliveryIcon");
 
-export const dropdownButton1 = document.getElementById("dropdownButton1");
+export const btnDropdown1 = document.getElementById("btnDropdown1");
 export const dropdownContent1 = document.getElementById("dropdownContent1");
 export const selectAllCheckbox = document.getElementById("selectAllCheckbox");
 export const dropdownTitle1 = document.getElementById("dropdownTitle1");
 
-export const dropdownButton2 = document.getElementById("dropdownButton2");
+export const btnDropdown2 = document.getElementById("btnDropdown2");
 export const dropdownContent2 = document.getElementById("dropdownContent2");
 export const dividerLine2 = document.getElementById("dividerLine2");
