9bb24eaf684a8f81fcba0bd21ceb962dfc7ce363
Maria Arkhipova <marias1403@yandex.ru>
2023-09-30 16:31:15 +0900

    fix: FormValidator

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -11,12 +11,16 @@ export default class FormValidator {
   _showInputError = (input) => {
     const error = this._formElement.querySelector(`#${input.id}-error`);
     input.classList.add(this._config.inputErrorClass);
-    const errorMessage = input.validity.valueMissing
-      ? this._emptyFieldErrorMessages[input.id]
-      : this._invalidEmailErrorMessages[input.id] || input.validationMessage;
     error.classList.add(this._config.errorClass);
-    error.textContent = errorMessage;
+    error.textContent = this._invalidEmailErrorMessages[input.id];
   };
+
+  _showEmptyInputError = (input) => {
+    const error = this._formElement.querySelector(`#${input.id}-error`);
+    input.classList.add(this._config.inputErrorClass);
+    error.classList.add(this._config.errorClass);
+    error.textContent = this._emptyFieldErrorMessages[input.id];
+  }
 
   _hideInputError = (input) => {
     const error = this._formElement.querySelector(`#${input.id}-error`);
@@ -33,45 +37,13 @@ export default class FormValidator {
     }
   };
 
-  _hasInvalidInputs = () => {
-    return this._inputs.some((input) => {
-      return !input.validity.valid;
-    })
-  };
-
-  disableButton = () => {
-    this._buttonSubmit.classList.add(this._config.inactiveButtonClass);
-    this._buttonSubmit.disabled = true;
-  };
-
-  _enableButton = () => {
-    this._buttonSubmit.classList.remove(this._config.inactiveButtonClass);
-    this._buttonSubmit.disabled = false;
-  };
-
-  resetValidation() {
-    this._toggleButtonState();
-    this._inputs.forEach((inputElement) => {
-      this._hideInputError(inputElement);
-    });
-  }
-
   formatPhoneNumber(input) {
     const cleanedValue = input.value.replace(/\D/g, '');
     input.value = cleanedValue.replace(/(\d{1,2})(\d{3})(\d{3})(\d{2})(\d{2})/, '+$1 $2 $3 $4 $5');
     this._validateInput(input);
   }
 
-  _toggleButtonState = () => {
-    if (this._hasInvalidInputs()) {
-      this.disableButton();
-    } else {
-      this._enableButton();
-    }
-  };
-
   _setEventListeners = () => {
-    this._toggleButtonState();
     this._inputs.forEach((input) => {
       input.addEventListener('blur', () => {
         if (input.id === 'recipientPhoneNumber') {
@@ -79,14 +51,17 @@ export default class FormValidator {
         } else {
           this._validateInput(input);
         }
-        this._toggleButtonState();
       });
     });
   }
 
   enableValidation = () => {
-    this._formElement.addEventListener('submit', (evt) => {
-      evt.preventDefault();
+    this._buttonSubmit.addEventListener("click", () => {
+      this._inputs.forEach((input) => {
+        if (input.value === "") {
+          this._showEmptyInputError(input);
+        }
+      });
     });
     this._setEventListeners();
   };
