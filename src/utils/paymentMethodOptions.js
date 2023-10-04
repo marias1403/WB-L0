18e527cec3cd2def64aa9dd2fe05e659dc0e742e
Maria Arkhipova <marias1403@yandex.ru>
2023-10-04 18:00:45 +0900

    fix: icons

--- src/utils/paymentMethodOptions.js
+++ src/utils/paymentMethodOptions.js
@@ -1,29 +1,34 @@
+import mirIcon from "../images/mir.svg";
+import visaIcon from "../images/visa.svg";
+import mastercardIcon from "../images/mastercard.svg";
+import maestroIcon from "../images/maestro.svg";
+
 export const paymentMethodOptions = {
   "MIR": {
     number: "1234 56•• •••• 1234",
     validity: "01/30",
-    icon: "http://localhost:8080/eba16186e928324d190d.svg",
+    icon: mirIcon,
     alt: "Банковская карта Мир",
     cssClass: "credit-card-icon__mir-img card-image-result"
   },
   "VISA": {
     number: "1234 56•• •••• 1234",
     validity: "01/30",
-    icon: "http://localhost:8080/602f33ff75387def8179.svg",
+    icon: visaIcon,
     alt: "Банковская карта Visa",
     cssClass: "credit-card-icon__visa-img card-image-result"
   },
   "MasterCard": {
     number: "1234 56•• •••• 1234",
     validity: "01/30",
-    icon: "http://localhost:8080/21b9cba1526694232644.svg",
+    icon: mastercardIcon,
     alt: "Банковская карта MasterCard",
     cssClass: "credit-card-icon__mastercard-img card-image-result"
   },
   "Maestro": {
     number: "1234 56•• •••• 1234",
     validity: "01/30",
-    icon: "http://localhost:8080/878ba193a812788271d7.svg",
+    icon: maestroIcon,
     alt: "Банковская карта Maestro",
     cssClass: "credit-card-icon__maestro-img card-image-result"
   },
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -44,6 +45,14 @@ module.exports = {
       template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/images',
+          to: 'images',
+        },
+      ],
+    }),
   ]
 };
